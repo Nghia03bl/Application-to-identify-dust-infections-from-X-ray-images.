447a5ee0ff5085579e86e26af170a0112e230554
Nghia03bl <117901265+Nghia03bl@users.noreply.github.com>
2023-12-12 23:29:14 +0700

    main.py

--- main.py
+++ main.py
@@ -5,24 +5,24 @@ import numpy as np
 
 IMG_SIZE = 227
 
-class_list = {'0': 'NORMAL', '1': 'PNEUMONIA'}
+class_list = {'0': 'Bình thường', '1': 'Viêm Phổi'}
 
-st.title('Pneumonia prediction based on chest X-Ray image')
+st.title('Dự đoán viêm phổi dựa trên hình ảnh X-quang ngực')
 
 input = open('lrc_xray.pkl', 'rb')
 model = pkl.load(input)
 
-st.header('Upload chest X-Ray image')
-uploaded_file = st.file_uploader("Choose an image file", type=(['png', 'jpg', 'jpeg']))
+st.header('Tải lên ảnh X-quang ngực')
+uploaded_file = st.file_uploader("Chọn 1 file ảnh", type=(['png', 'jpg', 'jpeg']))
 
 if uploaded_file is not None:
     image = Image.open(uploaded_file)
-    st.image(image, caption='Test image')
+    st.image(image, caption='Ảnh thử')
 
-    if st.button('Predict'):
+    if st.button('Dự đoán'):
         image = image.resize((IMG_SIZE*IMG_SIZE*3, 1))
         feature_vector = np.array(image)
         label = str((model.predict(feature_vector))[0])
 
-        st.header('Result')
+        st.header('Kết quả')
         st.text(class_list[label])
